b94517f145f646d87248a52b7de800c8616b1908
Saurav Panthee <saurav.panthee@gmail.com>
2021-02-04 17:52:13 -0600

    React with Tailwind Config and React Router

---
+++ src/views/Landing/Landing.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Landing(props) {
+    return (
+        
+         <div className="w-full bg-gray-200 h-screen flex flex-col justify-center items-center">
+         Landing Page of new multi-million dollar startup
+        </div>
+    )
+}
+
+Landing.propTypes = {
+
+}
+
+export default Landing
+
