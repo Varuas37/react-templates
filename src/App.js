f037e2f6f237a3714eac3844be45f1848ab24c0b
Saurav Panthee <saurav.panthee@gmail.com>
2021-02-04 18:23:32 -0600

    Added Redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import history from './utils/history';
 import Landing from './views/Landing/Landing';
+//Redux Import
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
 function App() {
 	return (
-		<Router history={history}>
-			<Switch>
-				<Route exact path="/" component={Landing} />
-			</Switch>
-		</Router>
+		<Provider store={store}>
+			<Router history={history}>
+				<Fragment>
+					<Switch>
+						<Route exact path="/" component={Landing} />
+					</Switch>
+				</Fragment>
+			</Router>
+		</Provider>
 	);
 }
 
