ab4b72fcdba7e03d3fd8ade1ce07f62e22722049
Saurav Panthee <saurav.panthee@gmail.com>
2021-02-04 18:47:07 -0600

    Fixed Error with Redux

--- src/redux/reducers/alert.js
+++ src/redux/reducers/alert.js
@@ -1,7 +1,7 @@
-import {SET_ALERT, REMOVE_ALERT} from "../Action/types";
+import {SET_ALERT, REMOVE_ALERT} from "../action/types";
 
 const initialState =[];
-export default function (action,state= initialState){
+export default function (state= initialState,action){
     const {type,payload} = action;
     switch (type){
         case SET_ALERT:
